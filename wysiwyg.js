9d50fcc8227820766403cfd1c384701c8cf3b327
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-07-21 10:54:34 -0500

    IT IS FINISHED

--- wysiwyg.js
+++ wysiwyg.js
@@ -1,11 +1,6 @@
 /**
  * Created by beelarr on 7/19/17.
  */
-
-
-
-
-
 let famous_folks = [{
                         title: "Samurai",
                         name: "Tomoe Gozen",
@@ -24,60 +19,51 @@ let famous_folks = [{
                         lifespan :{
                             birth: 1900,
                             death:'never'}
-                    }
+                    },
+    {
+        title: 'Famous Dude',
+        name: 'Your Daddy',
+        bio: 'Grew up in the sticks',
+        image: 'http://www.respectmyplanet.org/publications/wp-content/uploads/2015/06/960x540_jed_clampett.jpg',
+        lifespan :{
+            birth: 1900,
+            death:'never'}
+    }
                     ];
 
+let card_div = document.querySelector('#container');
 
 
 
 
+famous_folks.forEach((item, index) => {
 
-
-famous_folks.forEach((item, index) =>{
-    let content_block = `<person class="person--${index+1}"><header>${item.title} aka ${item.name}</header>
+    let content_block = `<header>${item.title} aka ${item.name}</header>
     <section>${item.bio}</section><img src="${item.image}"><footer>Born in ${item.lifespan.birth}.  Died in ${item.lifespan.death}</footer>
-    </person>`;
+    `;
 
-    let card_div = document.querySelector('#container');
-    card_div.innerHTML += content_block;
+    let personEL = document.createElement('person');
+    personEL.innerHTML += content_block;
+    personEL.classList.add(`person--${index}`);
+    card_div.appendChild(personEL);
+    personEL.addEventListener('click', () => {
+        personEL.classList.toggle('dotted');
 
-});
+    let section = document.querySelector('section');
+    let input = document.querySelector('input');
+    let bio = document.querySelector('section');
+    personEL.addEventListener('click', (e) => {
+            input.focus();
+            let current_card = e.currentTarget;
+        input.addEventListener('keyup', input_handler());
 
+        function input_handler() {
+            bio = current_card.querySelector('section');
+            console.log(bio);
+            bio.innerText = input.value
 
-let person = document.querySelectorAll('person');
-
-person.forEach((e)=>{
-    e.addEventListener('click', ()=>{
-        e.classList.toggle('dotted')
+            }
+        });
     })
 });
 
-
-let input = document.querySelector('input');
-let bio = document.querySelector('section');
-
-person.forEach(()=> {
-    addEventListener('click', () => {
-        input.focus()
-    })
-
-})
-
-
-input.addEventListener('keyup', (e) => {
-    bio.innerHTML = e.target.value;
-});
-
-
-
-
-
-
-
-
-
-
-// click on person
-// focus input
-// relays input
-// to person clicked
